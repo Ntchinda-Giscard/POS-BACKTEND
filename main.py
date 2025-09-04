54d9274a57b54989ca1613514fe505f28547bc68
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-09-04 17:23:48 +0100

    Add command router in main folder

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from src.articles.controller import router as article_router
 from src.addresse.controller import router as address_router
 from src.command.controller import router as command_router
+from src.clients.controller import router as clients_router
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -17,6 +18,7 @@ app.add_middleware(
 app.include_router(article_router)
 app.include_router(address_router)
 app.include_router(command_router)
+app.include_router(clients_router)
 
 @app.get("/")
 def read_root():
---
+++ src/clients/serivce.py
@@ -0,0 +1,20 @@
+import sqlite3
+from typing import List
+from ..clients.model import ClientResponse
+
+def get_clients() -> List[ClientResponse]:
+    """Fetch clients from the database."""
+    result = []
+
+    sqlite_conn = sqlite3.connect("sagex3_seed.db")
+    cursor = sqlite_conn.cursor()
+    cursor.execute("SELECT BPCNUM_0, BPCNAM_0 FROM BPCUSTOMER")
+
+    for row in cursor.fetchall():
+        client = ClientResponse(
+            code=row[0],
+            name=row[1]
+        )
+        result.append(client)
+
+    return result

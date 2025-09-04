045a841e4698a863cca2a19ce358f58e1ce0e676
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-09-04 15:23:12 +0100

    Add article request model and controller for article creation

---
+++ src/addresse/service.py
@@ -0,0 +1,16 @@
+import sqlite3
+from typing import List
+from addresse.model import AddressInput, AddressRequest
+
+def get_adresse_vente() -> List[AddressRequest]:
+    """  """
+    result = []
+    
+    sqlite_conn = sqlite3.connect("sagex3_seed.db")
+    cursor = sqlite_conn.cursor()
+    cursor.execute("SELECT FCY_0, FCYNAM_0 FROM FACILITY")
+    rows = cursor.fetchall()
+    for row in rows:
+        result.append(AddressRequest(code=row[0], description=row[1]))
+    return result
+    

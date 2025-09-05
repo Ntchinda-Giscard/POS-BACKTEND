e2f464de3d9478862b0953ba25a68e1fa1746849
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-09-05 09:50:05 +0100

    Add command router in main folder

--- src/clients/model.py
+++ src/clients/model.py
@@ -3,3 +3,4 @@ from pydantic import BaseModel
 class ClientResponse(BaseModel):
     code: str
     name: str
+    cur: str
--- src/clients/serivce.py
+++ src/clients/serivce.py
@@ -8,12 +8,13 @@ def get_clients() -> List[ClientResponse]:
 
     sqlite_conn = sqlite3.connect("sagex3_seed.db")
     cursor = sqlite_conn.cursor()
-    cursor.execute("SELECT BPCNUM_0, BPCNAM_0 FROM BPCUSTOMER")
+    cursor.execute("SELECT BPCNUM_0, BPCNAM_0, CUR_0 FROM BPCUSTOMER")
 
     for row in cursor.fetchall():
         client = ClientResponse(
             code=row[0],
-            name=row[1]
+            name=row[1],
+            cur=row[2]
         )
         result.append(client)
 

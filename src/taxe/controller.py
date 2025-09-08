71f87294cfc46cf466b91f12e148236d79c6b44f
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-09-08 17:15:37 +0100

    Add endpoint to retrieve tax regime by customer code

--- src/taxe/controller.py
+++ src/taxe/controller.py
@@ -1,3 +1,14 @@
-from typing import List 
+from typing import List
+from src.taxe.service import get_regime_taxe 
 from .model import TaxeResponse
+from fastapi import APIRouter
 
+router = APIRouter(
+    prefix="/taxe",
+    tags=["taxe"]
+)
+
+@router.get("/regime", response_model=TaxeResponse)
+def read_regime_taxe(customer_code: str):
+
+    return get_regime_taxe(customer_code)

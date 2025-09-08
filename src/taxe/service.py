440526915be5b5b1d2013e9cf3e4fe6afc408fa8
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-09-08 16:18:05 +0100

    Implement get_regime_taxe function to fetch tax regime from the database

---
+++ src/taxe/service.py
@@ -0,0 +1,22 @@
+
+
+import sqlite3
+from ..taxe.model import TaxeResponse
+
+
+def get_regime_taxe(customer_code: str) -> TaxeResponse:
+    """Fetch tax regime from the database."""
+    # Simulated database fetch
+    sqlite3_conn = sqlite3.connect("sagex3_seed.db")
+    cursor = sqlite3_conn.cursor() 
+    cursor.execute("""
+        SELECT
+            "VACBPR_0"
+        FROM
+            "BPCUSTOMER"
+        WHERE
+            "BPCNUM_0" = ?
+                   """, (customer_code,))
+    code = cursor.fetchone()[0]
+    cursor.close()
+    return TaxeResponse(code=code)

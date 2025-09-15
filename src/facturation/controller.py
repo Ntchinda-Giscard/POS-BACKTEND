aa145660861c344c750aa570a2794efc797134bf
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-09-15 14:47:07 +0100

    get cond fac controller

--- src/facturation/controller.py
+++ src/facturation/controller.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from src.facturation.service import get_escomte, get_payment_methode
-from .model import Escomte, PayementMode
+from src.facturation.service import get_cond_fac, get_escomte, get_payment_methode
+from .model import CondFacResponse, Escomte, PayementMode
 
 router = APIRouter(
     prefix="/facture",
@@ -16,3 +16,8 @@ def read_payment_method(customer_code: str) -> PayementMode:
 def read_escompte(customer_code: str) -> Escomte:
 
     return get_escomte(customer_code)
+
+@router.get("/escomte", response_model=Escomte)
+def read_cond_fac(customer_code: str) -> CondFacResponse:
+
+    return get_cond_fac(customer_code)

a4e78a420b82009ff47afe9bbc414443ee4e4f3c
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-09-23 12:58:59 +0100

    refactor controller.py to include AppliedTaxInput and AppliedTaxResponse for tax code retrieval

--- src/taxe/controller.py
+++ src/taxe/controller.py
@@ -1,6 +1,6 @@
 from typing import List
-from src.taxe.service import get_regime_taxe 
-from .model import TaxeResponse
+from src.taxe.service import get_applied_tax, get_regime_taxe 
+from .model import AppliedTaxInput, AppliedTaxResponse, TaxeResponse
 from fastapi import APIRouter
 
 router = APIRouter(
@@ -12,3 +12,9 @@ router = APIRouter(
 def read_regime_taxe(customer_code: str):
 
     return get_regime_taxe(customer_code)
+
+
+@router.post("/applied", response_model=AppliedTaxResponse)
+def get_taxe_code(criteria: AppliedTaxInput):
+
+    return get_applied_tax(criteria)

bbd62e0fbcdf3ec590ca9f7af309c53c1800c513
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-09-29 13:30:17 +0100

    fix: correct SQL query syntax in get_element_facturation function

--- src/command/model.py
+++ src/command/model.py
@@ -1,6 +1,26 @@
-from typing import Optional
+from typing import Any, Dict, List, Optional
 from pydantic import BaseModel
 
 class CommandTypeRRequest(BaseModel):
     code: str
     description: str
+
+
+class LigneCommande(BaseModel):
+    num_comd: str
+    item_code: str
+    prix_net_ht: str
+    prix_net_ttc: str
+    free_items: List[Dict[str, Any]] = None # type: ignore
+
+
+class CreateCommandRequest(BaseModel):
+    num_comd: str
+    currency: str
+    client_comd: str
+    client_payeur: str
+    client_facture: str
+    total_ht: float
+    total_ttc: float
+    valo_ht: float
+    valo_ttc: float

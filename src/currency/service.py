aabc60ed243e90965dc1359391d3992642964c53
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-11-20 16:50:19 +0100

    Enhance email functionality by adding zip file cleanup and improving attachment handling

--- src/currency/service.py
+++ src/currency/service.py
@@ -1,9 +1,12 @@
 import sqlite3
 from ..currency.model import CurrencyResponse
+from ...database.sync_data import sync_data_new
 
 def get_commande_currrency(customer_code: str) -> CurrencyResponse:
 
-    sqlite_conn = sqlite3.connect("sagex3_seed.db")
+    db_path = ""
+    db_path = sync_data_new()
+    sqlite_conn = sqlite3.connect(db_path) # type: ignore
 
     cursor = sqlite_conn.cursor()
     cursor.execute("SELECT CUR_0 FROM BPCUSTOMER WHERE BPCNUM_0 = ? ", (customer_code,))

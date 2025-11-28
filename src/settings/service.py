eff5b88856d33ff8941ac63548a0ab132521b4bf
Ntchinda-Giscard <ntchinda1998@gmail.com>
2025-11-28 11:07:08 +0100

    setup logging for

---
+++ src/settings/service.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s - %(name)s - %(funcName)s - %(lineno)d - %(threadName)s',
+    handlers=[
+        logging.StreamHandler(sys.stdout),
+        logging.FileHandler('fastapi.log')
+    ]
+)
+
+
